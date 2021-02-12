5075fac224f4593bd61f56d9a0e62eeff7b60702
21193066 <21193066@DSN1271158612.cfp127aluno.local>
2021-02-12 16:59:19 -0200

    configurei e implementei o pisca led no pino RD7

--- main.c
+++ main.c
@@ -7,7 +7,20 @@
 
 
 #include <xc.h>
+#include "config.h"
 
-void main(void) {
-    return;
+void main(void)
+{
+    TRISDbits.TRISD7 = 0;
+   
+    while(1)
+    {
+      PORTDbits.RD7 = 1;
+      __delay_ms (500);
+      PORTDbits.RD7 = 0;
+      __delay_ms (500);
+    }
+     
+            
+ 
 }
